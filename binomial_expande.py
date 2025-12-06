b290b01a1ecc2dfb21897bfc27b9c8c5cccd39d4
AinaGavsta <155564460+AinaGavsta@users.noreply.github.com>
2025-12-06 00:21:09 +0000

    Binomial expansion program
    Small program I made for the casio cg-50 that calculates the coefficient of X^y.
    The casio cg-50 supports basic and micropython and as I don't know basic yet this program has been made in micropython.

---
+++ binomial_expande.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n < 0:
+        return 'bad'
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+
+exponent_bracket=int(input(f'Enter the exponent of the bracket'))
+
+for x in range(exponent_bracket+1):
+    print((factorial(exponent_bracket))/((factorial(x))*(factorial((exponent_bracket-x)))))
